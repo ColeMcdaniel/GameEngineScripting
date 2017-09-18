7846f0747ff37293472504eaafd6d8794812a43b
ColeMcdaniel <32074076+ColeMcdaniel@users.noreply.github.com>
2017-09-18 15:32:37 -0500

    demo

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Debug.Log("Horizontal input: " + horizontalInput);
+
+        float speed = 0.2f;
+
+        transform.Translate(speed * horizontalInput, 0, 0);
+
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            transform.Translate(0, 6, 0);
+        }
+    }
+}
